ee84428c0f1cd94a0d879c78e13cea709fc2e2ab
Caleb York <caleb.york@fwcsstudents.org>
2024-09-25 13:17:41 -0400

    Add files via upload

---
+++ York_TIY_8_1.py
@@ -0,0 +1,7 @@
+def display_message(message):
+    message = f"I'm learning how to use {message}s."
+    return message
+
+message = "function"
+
+print(display_message(message))
---
+++ York_TIY_8_2.py
@@ -0,0 +1,7 @@
+def favorite_book(title):
+    message = f"My favorite book series is {title}."
+    return message
+
+book = "Guardian's of Ga'Hoole"
+
+print(favorite_book(book))
